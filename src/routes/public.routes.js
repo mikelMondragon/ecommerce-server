451111ec6aa19cb2d3412a4cda3ca6273bcc0705
mikelMondragon <132443700+mikelMondragon@users.noreply.github.com>
2025-07-05 12:05:34 +0200

    validations

--- src/routes/public.routes.js
+++ src/routes/public.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { check } = require('express-validator');
+const { check, param } = require('express-validator');
 const { validateInput } = require("../middlewares/validateInput.middleware")
 const upload = require("../middlewares/upload.middleware")
 
@@ -14,7 +14,9 @@ const router = new Router();
 router.get('/', getAllProducts);//productos?category=&color=negro&precioMax=100
 
 //GET: http://localhost:5000/api/v1/products/:id
-router.get('/:id', getProductById);
+router.get('/:id', [
+    param("id", "need valid id").isMongoId()
+], getProductById);
 
 //POST: http://localhost:5000/api/v1/products
 router.post('/', [
@@ -42,10 +44,31 @@ router.post('/', [
 
 //PUT: http://localhost:5000/api/v1/products/:id
 router.put('/:id', [
-    upload.any()
+    upload.any(),
+    // param("id", "need valid id").isMongoId(),
+    check("name", "empty name").notEmpty()
+        .isString()
+        .withMessage("name must be a string")
+        .isLength({ min: 2, max: 50 })
+        .withMessage("The name should have a length between 2 and 50"),
+    check("category", "no category").notEmpty()
+        .isString()
+        .withMessage("category must be a string"),
+    check("description", "description empty").notEmpty()
+        .isString()
+        .withMessage("description must be a string"),
+    check("price", "price is required").notEmpty()
+        .isFloat({ min: 0 })
+        .withMessage("price must be a numeric value and min 0"),
+    check("stock", "stock should have a value").notEmpty()
+        .isInt({ min: 0 })
+        .withMessage("stock must be a numeric value and min 0"),
+    validateInput
 ], updateProduct);
 
 //DELETE: http://localhost:5000/api/v1/products/:id
-router.delete('/:id', deleteProduct);
+router.delete('/:id', [
+    param("id", "need valid id").isMongoId()
+], deleteProduct);
 
 module.exports = router;
